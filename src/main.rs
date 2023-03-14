0dc1241b2c8e3117f17de2e50855fcbf07f3485c
Mike Rohner <mrohner94@hotmail.com>
2023-03-14 19:19:25 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,25 @@ fn main() {
     b.push(1);
     //a.push(1);
 
-    println!("{:?}", b)
+    println!("{:?}", b);
+
+    let mut x = 5;
+    {
+        let y = &mut x;
+
+        *y = 7;
+    }
+    
+
+    // x = 5;
+
+    println!("{:?}", x);
+
+    let mut n = vec![];
+    let i = &mut n;
+
+    i.push(1);
+    println!("{:?}", i);
+
+
 }
